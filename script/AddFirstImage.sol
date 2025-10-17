be4cb30f3777e0ef9d19b6fbaab8990b3412f380
andrew <ajm2dr@virginia.eud>
2025-10-17 00:28:10 -0400

    --Updated to Moon

--- script/AddFirstImage.sol
+++ script/AddFirstImage.sol
@@ -5,17 +5,22 @@ import {Script, console2} from "forge-std/Script.sol";
 import {FlipEngine} from "../src/FlipEngine.sol";
 
 contract AddFirstImage is Script {
-    function run(uint256 chunkIndex) public {  // Add chunkIndex parameter
+    function run() public {  // Add chunkIndex parameter
+     //   function run(uint256 chunkIndex) public {  // Add chunkIndex parameter
+     uint256 chunkIndex = 0;
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         
-        address deployedAddress = 0x471712732542Ea2b3BB0a7da11B865E328d65E99;
-        address flipEngineAddress = 0x3f902bbdeD1C66f3259695f85505273C11bC1146;
+      //  address deployedAddress = 0x471712732542Ea2b3BB0a7da11B865E328d65E99;
+      //  address flipEngineAddress = 0x3f902bbdeD1C66f3259695f85505273C11bC1146;
+
+        address deployedAddress = 0x5D33bf955F46Cf194ae742d5A1A6f4ADC50f118e;
+        address flipEngineAddress = 0x457b961794d9F1037C63c3dd09E588Cb93567FAa;
         FlipEngine flipEngine = FlipEngine(flipEngineAddress);
         
         string memory firstImagePath = "src/1.txt";
         string memory firstImageContent = vm.readFile(firstImagePath);
         
-        uint256 chunkSize = 8000;
+        uint256 chunkSize = 20000;
         uint256 contentLength = bytes(firstImageContent).length;
         
         require(chunkIndex * chunkSize < contentLength, "Chunk index out of bounds");
@@ -30,7 +35,7 @@ contract AddFirstImage is Script {
         console2.log("Processing chunk %d", chunkIndex);
         
         vm.startBroadcast(deployerPrivateKey);
-        flipEngine.addFirstImageChunk(deployedAddress, 19, chunkIndex, chunk);
+        flipEngine.addFirstImageChunk(deployedAddress, 16, chunkIndex, chunk);
         vm.stopBroadcast();
     }
     
--- script/MintTokenAndSetId.sol
+++ script/MintTokenAndSetId.sol
@@ -2,19 +2,19 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {IAWYAExtension} from "../src/IAWYAExtension.sol";
+import {MoonExtension} from "../src/MoonExtension.sol";
 
 contract Mint is Script {
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address extensionAddress = 0x471712732542Ea2b3BB0a7da11B865E328d65E99;
+        address extensionAddress = 0x2F794e3d36Ca796291815FB43F1FF424bf9223dF;
         address wallet = 0xF1Da6E2d387e9DA611dAc8a7FC587Eaa4B010013;
-        uint256 tokenId = 19;
+        uint256 tokenId = 27;
 
         vm.startBroadcast(deployerPrivateKey);
         
         // Create an instance of the contract using the interface
-        IAWYAExtension extension = IAWYAExtension(extensionAddress);
+        MoonExtension extension = MoonExtension(extensionAddress);
         
         // Call the functions directly
         extension.mint(wallet);
